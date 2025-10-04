f0de53bdc0bf2030ecfc7b3c253809ed51f2cbba
mickeysrishti <Mukeshpathak345@gmail.com>
2025-10-04 21:45:19 +0530

    code added

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,10 @@ import cors from "cors";
 import connectDB from "./database/db.js";
 import userRoute from "./routes/user.routes.js";
 import expenseRoute from "./routes/expense.route.js";
-import path from "path"
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({});
 
